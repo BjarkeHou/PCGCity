fdc3e484692c80947f03061d52a185fa71400c06
Oragada <Oragada@gmail.com>
2014-11-14 12:46:40 +0100

    Added Clone and Equals to Point2i

--- src/util/Point2i.java
+++ src/util/Point2i.java
@@ -1,7 +1,5 @@
 package util;
 
-import javax.vecmath.Tuple2d;
-
 public class Point2i {
 	public int x;
 	public int y;
@@ -13,5 +11,21 @@ public class Point2i {
 	
 	public double distanceTo(Point2i p) {
 		return Math.sqrt(Math.abs((double)x-(double)p.x))+Math.abs((double)y-(double)p.y);
+	}
+	
+	public Point2i clone(){
+		return new Point2i(x,y);
+		
+	}
+	
+	public boolean equals(Object o){
+		if(!(o instanceof Point2i)){
+			return false;
+		}
+		Point2i p = (Point2i) o;
+		if(p.x == x && p.y == y){
+			return true;
+		}
+		return false;
 	}
 }
