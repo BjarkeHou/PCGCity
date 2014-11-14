6d79c4b8de70a4b6f826b1c780e93394d3657eeb
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-11-14 12:27:58 +0100

    Added a minimalistic GUI for some test.. Tested MapHandler, which loads in a map perfectly from a given path..

---
+++ src/util/RuleHandler.java
@@ -0,0 +1,9 @@
+package util;
+
+public class RuleHandler {
+	// This class will consist of static methods, to load in JSON files containings rules.
+	
+	// Load in JSON file
+	// Parse JSON to a set of rules with requirements
+	// Return those requirements
+}
