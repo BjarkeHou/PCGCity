0c7929ab595507e655eb3b8df3ae64f8d069a1c5
Bjarke Hou Kammersgaard <mail@bjarkehou.dk>
2014-11-14 13:02:11 +0100

    GUI now shows an image of the loaded map.

--- src/gui/AppWindow.java
+++ src/gui/AppWindow.java
@@ -1,19 +1,27 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JEditorPane;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import com.sun.media.jai.opimage.AddCollectionCRIF;
 
 import controller.Controller;
-import javax.swing.JEditorPane;
-import java.awt.Insets;
-import javax.swing.JLabel;
-import java.awt.Font;
 
 public class AppWindow {
 
@@ -52,9 +60,9 @@ public class AppWindow {
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 204, 0};
+		gridBagLayout.columnWidths = new int[]{0, 204, 79, 0};
 		gridBagLayout.rowHeights = new int[]{53, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		frame.getContentPane().setLayout(gridBagLayout);
 		
@@ -67,6 +75,14 @@ public class AppWindow {
 		gbc_lblLoadAMap.gridy = 0;
 		frame.getContentPane().add(lblLoadAMap, gbc_lblLoadAMap);
 		
+		final JLabel mapLabel = new JLabel("");
+		GridBagConstraints gbc_mapLabel = new GridBagConstraints();
+		gbc_mapLabel.gridheight = 2;
+		gbc_mapLabel.insets = new Insets(0, 0, 5, 0);
+		gbc_mapLabel.gridx = 2;
+		gbc_mapLabel.gridy = 0;
+		frame.getContentPane().add(mapLabel, gbc_mapLabel);
+		
 		JButton btnLoadMap = new JButton("Load");
 		GridBagConstraints gbc_btnLoadMap = new GridBagConstraints();
 		gbc_btnLoadMap.insets = new Insets(0, 0, 0, 5);
@@ -77,6 +93,7 @@ public class AppWindow {
 		final JEditorPane dtrpnPathToMap = new JEditorPane();
 		dtrpnPathToMap.setToolTipText("Path to map ...");
 		GridBagConstraints gbc_dtrpnPathToMap = new GridBagConstraints();
+		gbc_dtrpnPathToMap.insets = new Insets(0, 0, 0, 5);
 		gbc_dtrpnPathToMap.fill = GridBagConstraints.BOTH;
 		gbc_dtrpnPathToMap.gridx = 1;
 		gbc_dtrpnPathToMap.gridy = 1;
@@ -86,8 +103,22 @@ public class AppWindow {
 			
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				if(!dtrpnPathToMap.getText().isEmpty())
+				if(!dtrpnPathToMap.getText().isEmpty()) {
 					con.loadMapOnPath(dtrpnPathToMap.getText());
+					
+					BufferedImage map;
+					try {
+						map = ImageIO.read(new File(dtrpnPathToMap.getText()));
+						
+						mapLabel.setIcon(new ImageIcon(map.getScaledInstance(400, 400, Image.SCALE_SMOOTH)));
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+					
+				}	
 			}
 		});
 	}
